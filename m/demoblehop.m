3002162dc3e9fc2f3bfa769d0287715b5e0ad4e8
Leo Wang <superwanghb@gmail.com>
2019-02-20 22:20:04 +0800

    add demobelhop

--- m/demoblehop.m
+++ m/demoblehop.m
@@ -2,5 +2,7 @@ clear all;
 clc;
 fid = fopen('onehop_200M.pcm','r','b');
 sig = fread(fid,'int16','l')';
+fclose(fid);
 fs = 200e6;
 fftlen = 1024;
+[fc,bw,peak_2_avg] = single_hop_est(sig,fs,fftlen);
